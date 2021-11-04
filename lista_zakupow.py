17964f979dda14f61c8313d9224076e5defa715f
Gocek <gocek89@gmail.com>
2021-11-04 17:17:46 +0100

    Add lista_zakupow.py

---
+++ lista_zakupow.py
@@ -0,0 +1,16 @@
+
+shoping_list = {
+    "piekarnia": ["chleb", "bułka tarta", "bułki"],
+    "warzywniak": ["pomidory", "ogórek", "ziemniaki"],
+    "zabka": ["mleko", "masło orzechowe", "chipsy"]}
+sum = 0
+for kay, value in shoping_list.items():
+    for v in range(len(value)):
+        value[v] = value[v].capitalize()
+    print(
+        f"Idę do {kay.capitalize()} ,kupuję tu następujące rzeczy: {value}")
+
+    x = len(value)
+    sum += x
+
+print(f"Suma wszystkich produktów wynosi {sum}")
